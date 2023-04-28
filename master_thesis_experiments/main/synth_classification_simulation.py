fixes in dataset generation, iw computation and dumping of results

master_thesis_experiments/main/synth_classification_simulation.py
@@ -21,6 +21,27 @@ from master_thesis_experiments.active_learning.first_k_sampling import FirstKSam
 
 class SynthClassificationSimulation(Simulation):
 
+    def __init__(
+            self,
+            name,
+            generator,
+            strategies,
+            results_dir,
+            n_samples,
+            estimator_type
+    ):
+        super().__init__(
+            name,
+            generator,
+            strategies,
+            results_dir,
+            n_samples,
+            estimator_type
+        )
+
+        self.mean_values = None
+        self.cov_values = None
+
     def generate_dataset(self,
                          n_concepts,
                          concept_size,
@@ -36,26 +57,41 @@ class SynthClassificationSimulation(Simulation):
         n = self.generator.size
         triangular_size = int(n * (n + 1) / 2)
 
+        self.mean_values = [
+            np.random.uniform(0, 8, self.generator.size) for _ in range(self.generator.n_classes)
+        ]
+        self.cov_values = [
+            np.random.uniform(6, 9, triangular_size) for _ in range(self.generator.n_classes)
+        ]
+
+        self.generator.mean_values = self.mean_values
+        self.generator.cov_values = self.cov_values
+
+        alpha = 3
+        # the higher the alpha, the more balanced the prior probabilities
+        self.generator.prior_probs = np.random.dirichlet(alpha * np.ones(self.generator.n_classes))
+
         for i in range(n_concepts):
             self.concept_mapping['concept_' + str(i)] = {}
 
-            self.generator.mean_values = [
-                np.random.uniform(0, 8, self.generator.size) for _ in range(self.generator.n_classes)
-            ]
-            self.generator.cov_values = [
-                np.random.uniform(6, 9, triangular_size) for _ in range(self.generator.n_classes)
-            ]
+            # perturbation of the means and cov matrices
+            mean_noises = [np.random.uniform(-0.1, 0.1, self.generator.size) for _ in range(self.generator.n_classes)]
+            cov_values_noises = [np.random.uniform(
+                -0.5, 0.5, triangular_size
+            ) for _ in range(self.generator.n_classes)]
+
+            for class_ in range(self.generator.n_classes):
+                self.generator.mean_values[class_] = self.mean_values[class_] + mean_noises[class_]
+                self.generator.cov_values[class_] = self.cov_values[class_] + cov_values_noises[class_]
 
             for j in range(self.generator.n_classes):
                 self.concept_mapping['concept_' + str(i)]['class_' + str(j)] = multivariate_normal(
                     self.generator.mean_values[j], self.generator.covariance_matrices[j]
                 )
-            n_classes = self.generator.n_classes
-            alpha = 3
-            # the higher the alpha, the more balanced the prior probabilities
-            prior_probs = np.random.dirichlet(alpha * np.ones(n_classes))
-            self.generator.prior_probs = prior_probs
-            self.prior_probs_per_concept.append(prior_probs)
+
+            # perturbation of the prior probabilities
+            # self.generator.prior_probs = something
+            self.prior_probs_per_concept.append(self.generator.prior_probs)
 
             if i != n_concepts - 1:
                 dataset = self.generator.generate(concept_size)
@@ -99,24 +135,28 @@ class SynthClassificationSimulation(Simulation):
             )
             self.strategy_instances.append(strategy_instance)
 
-            new_concept_list = strategy_instance.run()
+            n_samples = self.n_samples
+            while n_samples > 0:
+                new_concept_list = strategy_instance.run()
 
-            # compute post-AL weights
-            iw_handler = IWHandler(
-                concept_mapping=simulation.concept_mapping,
-                concept_list=new_concept_list,
-                estimator_type=simulation.estimator_type,
-                prior_class_probabilities=simulation.prior_probs_per_concept[:-1]
-            )
+                # compute post-AL weights
+                iw_handler = IWHandler(
+                    concept_mapping=simulation.concept_mapping,
+                    concept_list=new_concept_list,
+                    estimator_type=simulation.estimator_type,
+                    prior_class_probabilities=simulation.prior_probs_per_concept[:-1]
+                )
+                n_selected_samples = self.n_samples - n_samples + 1
+                self.strategy_post_AL_weights[(strategy_instance.name, n_selected_samples)] = iw_handler.run_weights().tolist()
 
-            self.strategy_post_AL_weights[strategy_instance.name] = iw_handler.run_weights().tolist()
+                n_samples -= 1
 
 
 if __name__ == '__main__':
 
-    N_EXPERIMENTS = 10
+    N_EXPERIMENTS = 1
 
-    N_FEATURES = 4
+    N_FEATURES = 2
     N_CLASSES = 3
 
     N_CONCEPTS = 5
@@ -124,7 +164,7 @@ if __name__ == '__main__':
     LAST_CONCEPT_SIZE = 60
 
     simulation = SynthClassificationSimulation(
-        name='synth_classification',
+        name='synth_classification_fixed_dataset_and_samples',
         generator=SynthClassificationGenerator(
             n_features=N_FEATURES,
             n_outputs=1,
@@ -133,19 +173,20 @@ if __name__ == '__main__':
         strategies=[
             UncertaintySpreadingStrategy,
             RandomSamplingStrategy,
-            FirstKSamplingStrategy
+            # FirstKSamplingStrategy
         ],
         results_dir=get_root_level_dir('results'),
         n_samples=10,
         estimator_type=MultivariateNormalEstimator
     )
 
+    simulation.generate_dataset(
+        n_concepts=N_CONCEPTS,
+        concept_size=CONCEPT_SIZE,
+        last_concept_size=LAST_CONCEPT_SIZE
+    )
+
     for experiment in tqdm(range(N_EXPERIMENTS)):
-        simulation.generate_dataset(
-            n_concepts=N_CONCEPTS,
-            concept_size=CONCEPT_SIZE,
-            last_concept_size=LAST_CONCEPT_SIZE
-        )
 
         simulation.run()
 