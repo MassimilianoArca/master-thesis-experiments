ess computation fix

master_thesis_experiments/active_learning/base.py
@@ -18,13 +18,13 @@ logger = get_logger(__file__)
 
 class BaseStrategy:
     def __init__(
-            self,
-            concept_mapping,
-            concept_list,
-            n_samples,
-            prior_probs,
-            estimator_type: DensityEstimator(),
-            estimator_dataset: Optional[DataProvider] = None,
+        self,
+        concept_mapping,
+        concept_list,
+        n_samples,
+        prior_probs,
+        estimator_type: DensityEstimator(),
+        estimator_dataset: Optional[DataProvider] = None,
     ):
         self.concept_mapping = concept_mapping
         self.concept_list = concept_list
@@ -102,7 +102,7 @@ class BaseStrategy:
         for class_ in self.classes:
             dist = self.concept_mapping[self.current_concept.name][
                 "class_" + str(class_)
-                ]
+            ]
             pdfs.append(dist.pdf(X))
 
         norm_pdfs = [float(i) / sum(pdfs) for i in pdfs]
@@ -125,22 +125,25 @@ def custom_boundary_1(x, y):
 
 
 def custom_boundary_2(x, y):
-    return -(x) ** 2 + y - x + 4
+    return -((x) ** 2) + y - x + 4
 
 
 def rotate(x, y, theta):
-    return [x * np.cos(theta) - y * np.sin(theta), x * np.sin(theta) + y * np.cos(theta)]
+    return [
+        x * np.cos(theta) - y * np.sin(theta),
+        x * np.sin(theta) + y * np.cos(theta),
+    ]
 
 
 class BaseStrategyV3:
     def __init__(
-            self,
-            concept_list,
-            n_samples,
-            current_concept_extended,
-            concept_mapping=None,
-            rotation_angle=None,
-            shape_param=None,
+        self,
+        concept_list,
+        n_samples,
+        current_concept_extended,
+        concept_mapping=None,
+        rotation_angle=None,
+        shape_param=None,
     ):
         self.n_samples = n_samples
         self.past_concepts = concept_list[:-1]
@@ -228,7 +231,7 @@ class BaseStrategyV3:
             for class_ in self.classes:
                 dist = self.concept_mapping[self.current_concept.name][
                     "class_" + str(class_)
-                    ]
+                ]
                 pdfs.append(dist.pdf(X.reshape(1, -1)))
 
             norm_pdfs = np.array([float(i) / sum(pdfs) for i in pdfs]).flatten()
@@ -255,27 +258,31 @@ class BaseStrategyV3:
             x_1 = X[1]
 
             y_line = np.clip(
-                (x_0 ** 3) / self.shape_param - self.shape_param * x_0 + self.shape_param * np.sin(
-                    x_0 / self.shape_param), -80, 80)
+                (x_0**3) / self.shape_param
+                - self.shape_param * x_0
+                + self.shape_param * np.sin(x_0 / self.shape_param),
+                -80,
+                80,
+            )
             y = [x_1 > y_line]
             circle1 = np.where((x_0 - 0.5) ** 2 + (x_1 - 0.5) ** 2 < 1)[0]
             if circle1.shape[0] != 0:
                 circle1 = circle1[0]
-                y[circle1] = (~(np.mean(y[circle1]) > 0.5))
+                y[circle1] = ~(np.mean(y[circle1]) > 0.5)
 
             circle2 = np.where((x_0 + 0.7) ** 2 + (x_1 + 1) ** 2 < 1)[0]
             if circle2.shape[0] != 0:
                 circle2 = circle2[0]
-                y[circle2] = (~(np.mean(y[circle2]) > 0.5))
+                y[circle2] = ~(np.mean(y[circle2]) > 0.5)
             circle3 = np.where((x_0 - 1) ** 2 + (x_1 + 1) ** 2 < 1)[0]
             if circle3.shape[0] != 0:
                 circle3 = circle3[0]
-                y[circle3] = (~(np.mean(y[circle3]) > 0.5))
+                y[circle3] = ~(np.mean(y[circle3]) > 0.5)
 
             circle4 = np.where((x_0 + 0.2) ** 2 + (x_1 - 0.2) ** 2 < 0.5)[0]
             if circle4.shape[0] != 0:
                 circle4 = circle4[0]
-                y[circle4] = (~(np.mean(y[circle4]) > 0.5))
+                y[circle4] = ~(np.mean(y[circle4]) > 0.5)
 
             self.selected_sample[-1] = y[0].astype("int64")
 

master_thesis_experiments/active_learning/entropy_diversity_sampling.py
@@ -13,7 +13,10 @@ from sklearn.naive_bayes import GaussianNB
 from scipy.stats import entropy
 
 from master_thesis_experiments.active_learning.base import BaseStrategy, BaseStrategyV3
-from master_thesis_experiments.adaptation.density_estimation import DensityEstimator, MultivariateNormalEstimator
+from master_thesis_experiments.adaptation.density_estimation import (
+    DensityEstimator,
+    MultivariateNormalEstimator,
+)
 from master_thesis_experiments.handlers.importance_weights import IWHandler
 from master_thesis_experiments.simulator_toolbox.utils import get_logger
 
@@ -30,7 +33,7 @@ class EntropyDiversitySamplingStrategy(BaseStrategyV3):
         current_concept_extended,
         concept_mapping=None,
         rotation_angle=None,
-        shape_param=None
+        shape_param=None,
     ):
         super().__init__(
             concept_list=concept_list,
@@ -38,7 +41,7 @@ class EntropyDiversitySamplingStrategy(BaseStrategyV3):
             current_concept_extended=current_concept_extended,
             concept_mapping=concept_mapping,
             rotation_angle=rotation_angle,
-            shape_param=shape_param
+            shape_param=shape_param,
         )
         self.name = "EntropyDiversitySampling"
         self.model = GaussianNB()

master_thesis_experiments/handlers/importance_weights.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from typing import List
 
 import numpy as np
@@ -47,18 +48,24 @@ class IWHandler:
                 input_estimator = self.estimator_type(concept_id=concept.name)
                 input_estimator.fit(X)
                 conditional_estimator = GaussianNB()
-                conditional_estimator.fit(X, y)
+                conditional_estimator.fit(X, y.to_numpy().ravel())
 
                 self.past_input_estimators[concept.name] = input_estimator
                 self.past_conditional_estimators[concept.name] = conditional_estimator
 
     def estimate_current_concept(self):
-
         X, y = self.current_concept.get_split_dataset_v3()
         if self.current_conditional_estimator is None:
             self.current_conditional_estimator = GaussianNB()
-
-        self.current_conditional_estimator.fit(X, y)
+            self.current_conditional_estimator.partial_fit(
+                X, y.to_numpy().ravel(), classes=self.classes
+            )
+        else:
+            last_X = X.iloc[-1].to_frame().T
+            last_y = y.iloc[-1].to_frame().T
+            self.current_conditional_estimator.partial_fit(
+                last_X, last_y.to_numpy().ravel()
+            )
 
         if self.input_distribution_estimator is None:
             input_distribution_estimator = self.estimator_type(
@@ -66,6 +73,41 @@ class IWHandler:
             )
             input_distribution_estimator.fit(X)
             self.input_distribution_estimator = input_distribution_estimator
+
+    def compute_multiple_iw_weights(self):
+        n_concepts = len(self.past_concepts) + 1
+        n_samples = self.current_concept.get_dataset().shape[0] + np.sum(
+            [concept.get_dataset().shape[0] for concept in self.past_concepts]
+        )
+
+        pdfs = np.empty(shape=(n_concepts, n_samples))
+        past_concepts = deepcopy(self.past_concepts)
+        past_concepts.append(deepcopy(self.current_concept))
+
+        all_samples = np.concatenate([concept.get_dataset().to_numpy() for concept in past_concepts], axis=0)
+        all_features = all_samples[:, :-1]
+        all_classes = all_samples[:, -1].astype(int)
+
+        for concept_index, concept in enumerate(past_concepts):
+            if concept.name == self.current_concept.name:
+                input_estimator = self.input_distribution_estimator
+                conditional_estimator = self.current_conditional_estimator
+            else:
+                input_estimator = self.past_input_estimators[concept.name]
+                conditional_estimator = self.past_conditional_estimators[concept.name]
+
+            pdfs[concept_index] = input_estimator.pdf(all_features) * conditional_estimator.predict_proba(all_features)[
+                np.arange(all_features.shape[0]), all_classes]
+
+        target_pdfs = pdfs[-1]
+        sample_per_concept_proportions = np.array(
+            [concept.get_dataset().shape[0] for concept in past_concepts]) / n_samples
+        behavioral_pdfs = pdfs * sample_per_concept_proportions.reshape((-1, 1))
+        behavioral_pdfs = np.sum(behavioral_pdfs, axis=0)
+
+        is_weights = target_pdfs / behavioral_pdfs
+        sn_weights = is_weights / is_weights.sum()
+        return sn_weights
 
     def compute_past_concepts_probabilities(self):
         """
@@ -84,13 +126,14 @@ class IWHandler:
                     sample = X.loc[index].to_frame().T
                     label = y.loc[index].astype(int)
 
-                    conditional_estimator = self.past_conditional_estimators[concept.name]
+                    conditional_estimator = self.past_conditional_estimators[
+                        concept.name
+                    ]
                     input_estimator = self.past_input_estimators[concept.name]
 
-                    concept_joints[index] = (
-                            conditional_estimator.predict_proba(sample)[0][label]
-                            * input_estimator.pdf(sample)
-                    )
+                    concept_joints[index] = conditional_estimator.predict_proba(sample)[
+                                                0
+                                            ][label] * input_estimator.pdf(sample)
                 concept_joints = pd.DataFrame(concept_joints)
                 self.past_concepts_joints_probabilities.append(concept_joints)
 
@@ -113,10 +156,9 @@ class IWHandler:
                 conditional_estimator = self.current_conditional_estimator
                 input_estimator = self.input_distribution_estimator
 
-                concept_joints[index] = (
-                        conditional_estimator.predict_proba(sample)[0][label]
-                        * input_estimator.pdf(sample)
-                )
+                concept_joints[index] = conditional_estimator.predict_proba(sample)[0][
+                                            label
+                                        ] * input_estimator.pdf(sample)
             concept_joints = pd.DataFrame(concept_joints)
             self.current_concept_joints_probabilities.append(concept_joints)
 
@@ -132,32 +174,15 @@ class IWHandler:
     def run_weights(self):
         self.estimate_past_concepts()
         self.estimate_current_concept()
-        self.compute_past_concepts_probabilities()
-        self.compute_current_concept_probabilities()
 
-        weights_list = self.compute_weights()
-
-        current_concept_weights = np.ones(
-            shape=self.current_concept.get_dataset().shape[0]
-        )
-        current_concept_weights = pd.DataFrame(current_concept_weights)
-        weights_list.append(current_concept_weights)
-
-        imp_weights = pd.DataFrame()
-        for weights in weights_list:
-            imp_weights = pd.concat([imp_weights, weights], axis=0)
-
-        imp_weights = imp_weights.to_numpy()
-        return imp_weights.flatten()
+        weights_list = self.compute_multiple_iw_weights()
+        return weights_list
 
     def compute_effective_sample_size(self):
         imp_weights = self.run_weights()
-        numerator = np.sum(imp_weights) ** 2
-        denominator = np.sum(imp_weights ** 2)
-        ess = numerator / denominator
+        ess = 1 / np.sum(imp_weights ** 2)
         return ess
 
     def soft_reset(self):
-
         self.weights_per_concept = []
         self.current_concept_joints_probabilities = []

master_thesis_experiments/main/synth_classification_simulation_v3.py
@@ -4,16 +4,18 @@ import random
 from copy import deepcopy
 from datetime import datetime
 from pathlib import Path
+from collections import Counter
 
 import numpy as np
 import pandas as pd
 from scipy.stats import multivariate_normal
 from sklearn.datasets import make_blobs
-from sklearn.linear_model import SGDClassifier, Perceptron
+from sklearn.linear_model import SGDClassifier, Perceptron, PassiveAggressiveClassifier
 from sklearn.metrics import euclidean_distances
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import GaussianNB
 from tqdm import tqdm
+from incremental_trees.models.classification.streaming_rfc import StreamingRFC
 
 from master_thesis_experiments.active_learning.base import BaseStrategyV3
 from master_thesis_experiments.active_learning.entropy_diversity_sampling import (
@@ -25,15 +27,18 @@ from master_thesis_experiments.active_learning.entropy_sampling import (
 from master_thesis_experiments.active_learning.random_sampling_v3 import (
     RandomSamplingStrategyV3,
 )
-from master_thesis_experiments.adaptation.density_estimation import MultivariateNormalEstimator
+from master_thesis_experiments.adaptation.density_estimation import (
+    MultivariateNormalEstimator,
+)
 from master_thesis_experiments.handlers.importance_weights import IWHandler
 
 from master_thesis_experiments.simulator_toolbox.data_provider.base import DataProvider
 from master_thesis_experiments.simulator_toolbox.generator.synth_classification_generator import (
     SynthClassificationGenerator,
 )
-from master_thesis_experiments.simulator_toolbox.generator.synth_classification_generator_decision_boundary import \
-    SynthClassificationGeneratorDecisionBoundary
+from master_thesis_experiments.simulator_toolbox.generator.synth_classification_generator_decision_boundary import (
+    SynthClassificationGeneratorDecisionBoundary,
+)
 from master_thesis_experiments.simulator_toolbox.generator.synth_classification_generator_v2 import (
     SynthClassificationGeneratorV2,
 )
@@ -254,25 +259,34 @@ def custom_boundary_1(x, y):
 
 
 def custom_boundary_2(x, y):
-    return -(x) ** 2 + y - x + 4
+    return -((x) ** 2) + y - x + 4
 
 
 def rotate(x, y, theta):
-    return [x * np.cos(theta) - y * np.sin(theta), x * np.sin(theta) + y * np.cos(theta)]
+    return [
+        x * np.cos(theta) - y * np.sin(theta),
+        x * np.sin(theta) + y * np.cos(theta),
+    ]
 
 
-def generate_data(data_size=10000,  # before imbalance, approx
-                  x_min_max=2,
-                  shape_param_1=1,  # try values like 3,5,7,[10] and see how graph changes
-                  shape_param_2=0.5,  # try 5,10,15,20
-                  shape_param_3=2,  # try between 1-5
-                  shape_param_4=3,  # try 50,100,150,200
-                  ):
+def generate_data(
+        data_size=10000,  # before imbalance, approx
+        x_min_max=2,
+        shape_param_1=1,  # try values like 3,5,7,[10] and see how graph changes
+        shape_param_2=0.5,  # try 5,10,15,20
+        shape_param_3=2,  # try between 1-5
+        shape_param_4=3,  # try 50,100,150,200
+):
     min_x, max_x = -x_min_max, x_min_max
     interval = (max_x - min_x) / data_size
     x_0 = np.arange(min_x, max_x, interval)
-    y_line = np.clip((x_0 ** 3) / shape_param_4 - shape_param_1 * x_0 + shape_param_2 * np.sin(x_0 / shape_param_3),
-                     -80, 80)
+    y_line = np.clip(
+        (x_0 ** 3) / shape_param_4
+        - shape_param_1 * x_0
+        + shape_param_2 * np.sin(x_0 / shape_param_3),
+        -80,
+        80,
+    )
 
     x_1 = np.random.uniform(min(y_line) - 2, max(y_line) + 2, len(x_0))
     y = x_1 > y_line
@@ -633,14 +647,12 @@ class SynthClassificationSimulationV3:
                 )
 
         elif dataset_type == "decision_boundary":
-
             centers = generate_mean_values(
                 min_distance=3,
                 n_features=2,
                 n_classes=self.n_classes,
             )
             for i in range(n_concepts):
-
                 if i != n_concepts - 1:
                     X, _ = make_blobs(
                         n_samples=concept_size,
@@ -652,8 +664,12 @@ class SynthClassificationSimulationV3:
                     classified_labels = []
                     for point in X:
                         rotated_point = rotate(point[0], point[1], 0.5 * i)
-                        region_1 = custom_boundary_1(rotated_point[0], rotated_point[1]) >= 0
-                        region_2 = custom_boundary_2(rotated_point[0], rotated_point[1]) >= 0
+                        region_1 = (
+                                custom_boundary_1(rotated_point[0], rotated_point[1]) >= 0
+                        )
+                        region_2 = (
+                                custom_boundary_2(rotated_point[0], rotated_point[1]) >= 0
+                        )
 
                         if region_1 and region_2:
                             classified_labels.append(0.0)
@@ -663,6 +679,36 @@ class SynthClassificationSimulationV3:
                             classified_labels.append(2.0)
                         else:
                             classified_labels.append(3.0)
+
+                    while len(Counter(classified_labels).keys()) < 4:
+                        logger.debug(
+                            "Regenerating dataset, some class did not generate any sample"
+                        )
+                        X, _ = make_blobs(
+                            n_samples=last_concept_size + self.test_set_size + 10000,
+                            centers=centers,
+                            n_features=2,
+                            cluster_std=1.5,
+                        )
+
+                        classified_labels = []
+                        for point in X:
+                            rotated_point = rotate(point[0], point[1], 0.5 * i)
+                            region_1 = (
+                                    custom_boundary_1(rotated_point[0], rotated_point[1]) >= 0
+                            )
+                            region_2 = (
+                                    custom_boundary_2(rotated_point[0], rotated_point[1]) >= 0
+                            )
+
+                            if region_1 and region_2:
+                                classified_labels.append(0.0)
+                            elif not region_1 and region_2:
+                                classified_labels.append(1.0)
+                            elif not region_1 and not region_2:
+                                classified_labels.append(2.0)
+                            else:
+                                classified_labels.append(3.0)
                 else:
                     concept, _ = make_blobs(
                         n_samples=last_concept_size + self.test_set_size + 10000,
@@ -674,8 +720,12 @@ class SynthClassificationSimulationV3:
                     concept_labels = []
                     for point in concept:
                         rotated_point = rotate(point[0], point[1], 0.5 * i)
-                        region_1 = custom_boundary_1(rotated_point[0], rotated_point[1]) >= 0
-                        region_2 = custom_boundary_2(rotated_point[0], rotated_point[1]) >= 0
+                        region_1 = (
+                                custom_boundary_1(rotated_point[0], rotated_point[1]) >= 0
+                        )
+                        region_2 = (
+                                custom_boundary_2(rotated_point[0], rotated_point[1]) >= 0
+                        )
 
                         if region_1 and region_2:
                             concept_labels.append(0.0)
@@ -685,23 +735,61 @@ class SynthClassificationSimulationV3:
                             concept_labels.append(2.0)
                         else:
                             concept_labels.append(3.0)
-
-                    self.rotation_angle = 0.5 * i
-
                     X = concept[:last_concept_size]
                     classified_labels = concept_labels[:last_concept_size]
 
+                    while len(Counter(classified_labels).keys()) < 4:
+                        logger.debug(
+                            "Regenerating dataset, some class did not generate any sample"
+                        )
+                        concept, _ = make_blobs(
+                            n_samples=last_concept_size + self.test_set_size + 10000,
+                            centers=centers,
+                            n_features=2,
+                            cluster_std=1.5,
+                        )
+
+                        concept_labels = []
+                        for point in concept:
+                            rotated_point = rotate(point[0], point[1], 0.5 * i)
+                            region_1 = (
+                                    custom_boundary_1(rotated_point[0], rotated_point[1]) >= 0
+                            )
+                            region_2 = (
+                                    custom_boundary_2(rotated_point[0], rotated_point[1]) >= 0
+                            )
+
+                            if region_1 and region_2:
+                                concept_labels.append(0.0)
+                            elif not region_1 and region_2:
+                                concept_labels.append(1.0)
+                            elif not region_1 and not region_2:
+                                concept_labels.append(2.0)
+                            else:
+                                concept_labels.append(3.0)
+
+                        X = concept[:last_concept_size]
+                        classified_labels = concept_labels[:last_concept_size]
+
+                    self.rotation_angle = 0.5 * i
+
                     self.test_set = pd.DataFrame(
-                        concept[last_concept_size:last_concept_size + self.test_set_size],
+                        concept[
+                        last_concept_size: last_concept_size + self.test_set_size
+                        ],
                         columns=["X_0", "X_1"],
                     )
-                    self.test_set["y_0"] = concept_labels[last_concept_size:last_concept_size + self.test_set_size]
+                    self.test_set["y_0"] = concept_labels[
+                                           last_concept_size: last_concept_size + self.test_set_size
+                                           ]
 
                     self.current_concept_extended = pd.DataFrame(
                         concept[last_concept_size + self.test_set_size:],
                         columns=["X_0", "X_1"],
                     )
-                    self.current_concept_extended["y_0"] = concept_labels[last_concept_size + self.test_set_size:]
+                    self.current_concept_extended["y_0"] = concept_labels[
+                                                           last_concept_size + self.test_set_size:
+                                                           ]
 
                 dataset = pd.DataFrame(X, columns=["X_0", "X_1"])
                 dataset["y_0"] = classified_labels
@@ -710,7 +798,6 @@ class SynthClassificationSimulationV3:
                     DataProvider(name=f"concept_{i}", generated_dataset=dataset)
                 )
         elif dataset_type == "custom":
-
             for i in range(n_concepts):
                 if i != n_concepts - 1:
                     X = generate_data(
@@ -769,8 +856,12 @@ class SynthClassificationSimulationV3:
                         X = concept[:last_concept_size]
                         generated_classes = X["y_0"].unique()
 
-                    self.test_set = concept[last_concept_size:last_concept_size + self.test_set_size]
-                    self.current_concept_extended = concept[last_concept_size + self.test_set_size:]
+                    self.test_set = concept[
+                                    last_concept_size: last_concept_size + self.test_set_size
+                                    ]
+                    self.current_concept_extended = concept[
+                                                    last_concept_size + self.test_set_size:
+                                                    ]
 
                 self.concept_list.append(
                     DataProvider(name=f"concept_{i}", generated_dataset=X)
@@ -843,7 +934,6 @@ class SynthClassificationSimulationV3:
         self.clairvoyant_final_accuracy = clairvoyant.score(X_test, y_test)
 
         for strategy in self.strategies:
-
             iw_handler = IWHandler(
                 concept_list=deepcopy(self.concept_list),
                 estimator_type=MultivariateNormalEstimator
@@ -1038,7 +1128,7 @@ if __name__ == "__main__":
 
     N_CONCEPTS = 5
     CONCEPT_SIZE = 500
-    LAST_CONCEPT_SIZE = 5
+    LAST_CONCEPT_SIZE = 20
 
     TEST_SET_SIZE = 300
 
@@ -1046,8 +1136,8 @@ if __name__ == "__main__":
         name="synth_classification_v3",
         strategies=[
             RandomSamplingStrategyV3,
+            EntropySamplingStrategy,
             EntropyDiversitySamplingStrategy,
-            EntropySamplingStrategy
         ],
         result_dir=get_root_level_dir("results"),
         n_classes=N_CLASSES,
@@ -1060,7 +1150,7 @@ if __name__ == "__main__":
             n_concepts=N_CONCEPTS,
             concept_size=CONCEPT_SIZE,
             last_concept_size=LAST_CONCEPT_SIZE,
-            dataset_type="multivariate_normal",
+            dataset_type="decision_boundary",
         )
 
         simulation.run()